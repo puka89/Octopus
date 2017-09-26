7eeec3ce60ad772de0d28a76e5905cd595130955
puka89 <ludia.kud@abv.bg>
2017-09-26 22:35:20 +0300

    whater flow sensor test code

---
+++ test__YF-S201_whater_flow_sensor/test__YF-S201_whater_flow_sensor.ino
@@ -0,0 +1,26 @@
+int FlowPin = 2;
+double FlowRate;
+volatile int count;
+
+void setup() {
+  pinMode(FlowPin, INPUT);
+  attachInterrupt(0, Flow, RISING);
+  Serial.begin(9600);
+}
+void loop() { 
+  count = 0;
+  interrupts();
+  delay (1000);
+  noInterrupts();
+  
+  FlowRate = (count * 2.25);
+  FlowRate = FlowRate * 60;
+  FlowRate = FlowRate / 1000;
+
+  Serial.println(FlowRate);
+}
+
+void Flow()
+{
+   count++;
+}
